750a77060c34d8063d2f7ed58f4feeea51241680
Alexandre Morais <xandre2setembro2002@gmail.com>
2025-01-27 19:58:30 -0300

    Add jump and double jump functionalities

--- entities/player.gd
+++ entities/player.gd
@@ -2,8 +2,10 @@ extends CharacterBody3D
 
 @export var speed: int = 14
 @export var fall_acceleration: int = 75
+@export var jump_impulse: int = 20
 
 var target_velocity: Vector3 = Vector3.ZERO
+var can_double_jump: bool = true
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector3 = Vector3.ZERO
@@ -24,8 +26,17 @@ func _physics_process(delta: float) -> void:
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 
-	if not is_on_floor():
+	if not self.is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	else:
+		can_double_jump = true
 
-	velocity = target_velocity
-	move_and_slide()
+	self.velocity = target_velocity
+
+	if self.is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	elif can_double_jump and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+		can_double_jump = false
+
+	self.move_and_slide()
